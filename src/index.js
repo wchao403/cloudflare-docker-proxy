6d2fc758ceb21e7013276668171abbd63165c52f
Brook <69490236+wchao403@users.noreply.github.com>
2024-09-28 10:48:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.wchao.website": dockerHub,
+  "quay.wchao.website": "https://quay.io",
+  "gcr.wchao.website": "https://gcr.io",
+  "k8s-gcr.wchao.website": "https://k8s.gcr.io",
+  "k8s.wchao.website": "https://registry.k8s.io",
+  "ghcr.wchao.website": "https://ghcr.io",
+  "cloudsmith.wchao.website": "https://docker.cloudsmith.io",
+  "ecr.wchao.website": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.wchao.website": dockerHub,
 };
 
 function routeByHosts(host) {
